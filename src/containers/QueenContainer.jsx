32d058c9a4e9c0b6741ed0629b8952534aeaf550
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-04-29 15:00:05 -0700

    this

--- src/containers/QueenContainer.jsx
+++ src/containers/QueenContainer.jsx
@@ -6,6 +6,13 @@ const QueenPicker = () => {
   const [color, setColor] = useState('#FF0000');
   const [name, setName] = useState('red');
 
+  const state = {
+    id:'number',
+    name: 'string',
+    url: 'string',
+    loading:
+  }
+
   const handleChange = ({ target }) => {
     if(target.name === 'color') setColor(target.value);
     if(target.name === 'name') setName(target.value);
