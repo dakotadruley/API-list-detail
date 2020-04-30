3d294c3f30f79b63aecd5c07b43470c292335279
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-04-29 17:07:48 -0700

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const QueenDetail = ({ name, image, winner, missCongeniality, quote }) => (
+const Header = () => (
   <section>
-    <h2>{name}</h2>
-    <img src={image} />
-    <p>Winner? {winner}</p>
-    <p>MissCongeniality? {missCongeniality}</p>
-    <p>Quote: {quote}</p>
+    <h1>Meet the Queens</h1>
   </section>
 );
-QueenDetail.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  winner: PropTypes.bool.isRequired,
-  missCongeniality: PropTypes.bool.isRequired,
-  quote: PropTypes.string.isRequired
-};
-export default QueenDetail;
+
+export default Header;
